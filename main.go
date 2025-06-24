d67714c573187938cd87e3c17e52de2d51560748
abcdavk <xloot.mcpedl@gmail.com>
2025-06-24 14:37:02 +0700

    add script support and resource pack

--- main.go
+++ main.go
@@ -11,72 +11,149 @@ import (
 	"github.com/google/uuid"
 )
 
-type Modules struct {
+type ModuleTypeData struct {
 	Type    string `json:"type"`
 	UUID    string `json:"uuid"`
 	Version [3]int `json:"version"`
 }
 
-type Dependencies struct {
+type ModuleTypeScript struct {
+	Type        string `json:"type"`
+	UUID        string `json:"uuid"`
+	Version     [3]int `json:"version"`
+	Description string `json:"description"`
+	Lang        string `json:"language"`
+	Entry       string `json:"entry"`
+}
+
+type DependencyAddon struct {
 	UUID    string `json:"uuid"`
 	Version [3]int `json:"version"`
 }
 
+type DependencyScript struct {
+	ModuleName string `json:"module_name"`
+	Version    string `json:"version"`
+}
+
+type Header struct {
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	UUID         string `json:"uuid"`
+	Version      [3]int `json:"version"`
+	MinEngineVer [3]int `json:"min_engine_version"`
+}
+
 type ManifestBehavior struct {
-	FormatVersion int `json:"format_version"`
-	Header        struct {
-		Name         string `json:"name"`
-		Description  string `json:"description"`
-		UUID         string `json:"uuid"`
-		Version      [3]int `json:"version"`
-		MinEngineVer [3]int `json:"min_engine_version"`
-	} `json:"header"`
-	Modules      []Modules      `json:"modules"`
-	Dependencies []Dependencies `json:"dependencies"`
+	FormatVersion int    `json:"format_version"`
+	Header        Header `json:"header"`
+	Modules       []any  `json:"modules"`
+	Dependencies  []any  `json:"dependencies"`
+}
+
+type ManifestResources struct {
+	FormatVersion int    `json:"format_version"`
+	Header        Header `json:"header"`
+	Dependencies  []any  `json:"dependencies"`
 }
 
 func main() {
 	mnbp := ManifestBehavior{}
+	mnrp := ManifestResources{}
 
 	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("Addon Name: ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\n")
-		if len(input) != 0 {
-			mnbp.Header.Name = input
-			break
-		} else {
-			mnbp.Header.Name = "Addon Template"
-			break
-		}
+	fmt.Print("Addon Name: ")
+	addonNameInput, _ := reader.ReadString('\n')
+	addonNameInput = strings.TrimSuffix(addonNameInput, "\n")
+	if len(addonNameInput) != 0 {
+		mnbp.Header.Name = addonNameInput
+		mnrp.Header.Name = addonNameInput
+	} else {
+		mnbp.Header.Name = "Addon Template"
+		mnrp.Header.Name = "Addon Template"
 	}
 
-	for {
-		fmt.Print("Addon Description: ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\n")
-		if len(input) != 0 {
-			mnbp.Header.Description = input
-			break
-		} else {
-			mnbp.Header.Description = "Cool addon temolate!"
-			break
+	fmt.Print("Addon Description: ")
+	addonDescInput, _ := reader.ReadString('\n')
+	addonDescInput = strings.TrimSuffix(addonDescInput, "\n")
+	if len(addonDescInput) != 0 {
+		mnbp.Header.Description = addonDescInput
+		mnrp.Header.Description = addonDescInput
+	} else {
+		mnbp.Header.Description = "Cool addon template!"
+		mnrp.Header.Description = "Cool addon template!"
+	}
+
+	fmt.Print("Use script API? (yes/no): ")
+	useScriptInput, _ := reader.ReadString('\n')
+	useScriptInput = strings.TrimSuffix(useScriptInput, "\n")
+	if useScriptInput == "no" {
+
+	} else {
+		fmt.Print("Script API version (default: 2.0.0): ")
+		scriptVersionInput, _ := reader.ReadString('\n')
+		scriptVersionInput = strings.TrimSuffix(scriptVersionInput, "\n")
+
+		scriptDepend := DependencyScript{
+			ModuleName: "@minecraft/server",
+			Version:    scriptVersionInput,
 		}
+
+		if scriptVersionInput == "" {
+			scriptDepend.Version = "2.0.0"
+		}
+
+		mnbp.Dependencies = append(mnbp.Dependencies, scriptDepend)
+
+		fmt.Print("Script UI version (default: 2.0.0): ")
+		scriptUIVersionInput, _ := reader.ReadString('\n')
+		scriptUIVersionInput = strings.TrimSuffix(scriptUIVersionInput, "\n")
+
+		scriptUIDepend := DependencyScript{
+			ModuleName: "@minecraft/server-ui",
+			Version:    scriptUIVersionInput,
+		}
+
+		if scriptUIVersionInput == "" {
+			scriptUIDepend.Version = "2.0.0"
+		}
+
+		mnbp.Dependencies = append(mnbp.Dependencies, scriptUIDepend)
 	}
 
 	mnbp.FormatVersion = 2
 
 	mnbp.Header.UUID = uuid.NewString()
+	mnrp.Header.UUID = uuid.NewString()
+
 	mnbp.Header.Version = [3]int{1, 0, 0}
 	mnbp.Header.MinEngineVer = [3]int{1, 21, 0}
-	mnbp.Modules = append(mnbp.Modules, Modules{
+
+	mnrp.Header.Version = [3]int{1, 0, 0}
+	mnrp.Header.MinEngineVer = [3]int{1, 21, 0}
+
+	mnbp.Modules = append(mnbp.Modules, ModuleTypeData{
 		Type:    "data",
 		UUID:    uuid.NewString(),
 		Version: [3]int{1, 0, 0},
 	})
-	mnbp.Dependencies = append(mnbp.Dependencies, Dependencies{
-		UUID:    uuid.NewString(),
+
+	mnbp.Modules = append(mnbp.Modules, ModuleTypeScript{
+		Type:        "data",
+		UUID:        uuid.NewString(),
+		Version:     [3]int{1, 0, 0},
+		Description: "Script Resource",
+		Lang:        "javascript",
+		Entry:       "scripts/main.js",
+	})
+
+	mnbp.Dependencies = append(mnbp.Dependencies, DependencyAddon{
+		UUID:    mnrp.Header.UUID,
+		Version: [3]int{1, 0, 0},
+	})
+
+	mnrp.Dependencies = append(mnrp.Dependencies, DependencyAddon{
+		UUID:    mnbp.Header.UUID,
 		Version: [3]int{1, 0, 0},
 	})
 
@@ -90,4 +167,19 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	output, err = json.MarshalIndent(mnrp, "", "	")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile("RP/manifest.json", []byte(output), 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	println("Successfully created manifests!")
+	println("BP/manifest.json")
+	println("RP/manifest.json")
 }
