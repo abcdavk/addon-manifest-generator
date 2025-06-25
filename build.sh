fec9d62edbe6d8ffafc8f8f27c6e27e7dc5f7c43
abcdavk <xloot.mcpedl@gmail.com>
2025-06-25 14:00:09 +0700

    update build.sh add build for all platform

--- build.sh
+++ build.sh
@@ -3,15 +3,25 @@ if [ "$1" = "" ]; then
   echo 'Usage generate-manifest --[option]'
   echo ' ' 
   echo '  OS                option'
+  echo '  All               --all -a'
   echo '  Windows amd64     --windows -w'
   echo '  Linux             --linux -l'
   echo ' '
 fi
+if [ "$1" = "--all" -o "$1" = "-a" ]; then
+  echo Building executable for windows amd64 and linux
+  go build -o build/generate-manifest
+  GOOS=windows GOARCH=amd64 go build -o build/generate-manifest.exe
+  echo '  linux: build/generate-manifest'
+  echo '  windows: build/generate-manifest.exe'
+fi
 if [ "$1" = "--windows" -o "$1" = "-w" ]; then
   echo Building executable for windows amd64
   GOOS=windows GOARCH=amd64 go build -o build/generate-manifest.exe
+  echo '  windows: build/generate-manifest.exe'
 fi
 if [ "$1" = "--linux" -o "$1" = "-l" ]; then
   echo Building executable for linux
   go build -o build/generate-manifest
+  echo '  linux: build/generate-manifest'
 fi
