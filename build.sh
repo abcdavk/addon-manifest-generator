950aa589ecb2d758f90d050796cc794016b28b88
abcdavk <xloot.mcpedl@gmail.com>
2025-06-25 13:42:57 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
-go build -o build/generate-manifest
+#!/bin/bash
+if [ "$1" = "" ]; then
+  echo 'Usage generate-manifest --[option]'
+  echo ' ' 
+  echo '  OS                option'
+  echo '  Windows amd64     --windows -w'
+  echo '  Linux             --linux -l'
+  echo ' '
+fi
+if [ "$1" = "--windows" -o "$1" = "-w" ]; then
+  echo Building executable for windows amd64
+  GOOS=windows GOARCH=amd64 go build -o build/generate-manifest.exe
+fi
+if [ "$1" = "--linux" -o "$1" = "-l" ]; then
+  echo Building executable for linux
+  go build -o build/generate-manifest
+fi
